a8b1fc461ea554ab368738eca32a6765fcf06223
qjoshlawlor <joshlawlor1@gmail.com>
2022-11-18 12:35:22 -0500

    Added basic login/signup form. May need to tweak input fields  when add in functionality

--- bugbuddy/src/components/LoginForm/LoginForm.jsx
+++ bugbuddy/src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
 
+//BOOTSTRAP
+import Form from 'react-bootstrap/Form'
+import { Button, Container } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 
 const LoginForm = () => {
 
 
-    return(
-        <div>
-            <h1>Login page here</h1>
+    return (
+        <div className='loginMain'>
+            <br/>
+            <h2>Welcome back BugBuddy!</h2>
+            <br/>
+
+            <div className='formBox'>
+                <Container>
+
+                    <Card>
+                        <Form>
+                            <Form.Group classname='mb-3' controlId='formBasicEmail'>
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control type='email' placeholder='Enter Email' />
+                                <Form.Text size='sm' className='text-muted'>
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type='password' placeholder='Enter Password'/>
+
+                            </Form.Group>
+                            <Button variant='success' type='submit'>
+                                Login
+                            </Button>
+
+                        </Form>
+                    </Card>
+
+                </Container>
+
+
+            </div>
 
 
         </div>
--- bugbuddy/src/components/SignUpForm/SignUpForm.jsx
+++ bugbuddy/src/components/SignUpForm/SignUpForm.jsx
@@ -1,12 +1,59 @@
 import React from 'react'
 
-
+//BOOTSTRAP
+import {Container, Card} from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 const SignUpForm = () => {
 
 
     return(
         <div>
-            <h1>Sign Up page here</h1>
+            <br/>
+            <h1>Become a Bugbuddy!</h1>
+            <br/>
+            <div className='formBox'>
+                <Container>
+
+                    <Card>
+                        <Form>
+                        <Form.Group>
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type='email' placeholder='Enter Username' />
+                                <Form.Text size='sm' className='text-muted'>
+                                    Pick something unique!
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group classname='mb-3' controlId='formBasicEmail'>
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control type='email' placeholder='Enter Email' />
+                                <Form.Text size='sm' className='text-muted'>
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type='password' placeholder='Enter Password'/>
+
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control type='password' placeholder='Confirm Password'/>
+
+                            </Form.Group>
+                            <Button variant='success' type='submit'>
+                                Sign Up
+                            </Button>
+
+                        </Form>
+                    </Card>
+
+                </Container>
+
+
+            </div>
 
 
         </div>
